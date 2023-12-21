63783c1123abbc9c14a390102b85d31cd9e9cd37
larisa-mih <152610664+Larisa-mih@users.noreply.github.com>
2023-12-22 01:14:48 +0300

    Develop (#4)
    * change config.py
    
    * change main.py
    
    * change test_utils.py
    
    * change utils.py
    
    * перенос main.py в корень проекта
    
    * add 2 files json_tests

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+from config import OPERATIONS_PATH
+from src.utils import get_data, get_filtered_data, get_sorted_list, formate_operations_for_output
+
+
+def main():
+    all_operations = get_data(OPERATIONS_PATH)
+    filtered_operations = get_filtered_data(all_operations)
+    sorted_operations = get_sorted_list(filtered_operations)
+    last_five_operations = sorted_operations[:5]
+    for operation in last_five_operations:
+        print(formate_operations_for_output(operation))
+
+
+if __name__ == "__main__":
+    main()
