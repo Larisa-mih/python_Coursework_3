51fa12543c166f5d92a8c2bea107e7b7a052541e
larisa <larisa.mih2004@gmail.com>
2023-12-22 01:01:06 +0300

    change config.py

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 ROOT_DIR = os.path.dirname(__file__)
 OPERATIONS_PATH = os.path.join(ROOT_DIR, "src", "operations.json")
+TEST_OPERATIONS_PATH = os.path.join(ROOT_DIR, "tests", "operations_for_test.json")
+TEST_OPERATIONS_SORTED_PATH = os.path.join(ROOT_DIR, "tests", "operation_sorted_test.json")
+TEST_OPERATIONS_FILTERED_PATH = os.path.join(ROOT_DIR, "tests", "operation_filtered_test.json")
