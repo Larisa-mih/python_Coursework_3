050df4ddde554c032709076d1dfb8744a6773527
larisa <larisa.mih2004@gmail.com>
2023-12-22 01:01:49 +0300

    change test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,51 +1,83 @@
-from src.utils import number_format, date_format, get_data, get_sorted_list, get_filtered_data
+from config import TEST_OPERATIONS_PATH, TEST_OPERATIONS_SORTED_PATH, TEST_OPERATIONS_FILTERED_PATH
+from src.utils import (number_format, data_format, get_data, get_sorted_list,
+                       get_filtered_data, formate_operations_for_output)
 import json
-from config import ROOT_DIR
-import os
+
 def test_number_format():
     assert number_format("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
     assert number_format("Счет 35383033474447895560") == "Счет **5560"
 
-def test_date_format():
-    assert date_format("2018-06-30") == "30.06.2018"
+
+def test_data_format():
+    assert data_format("2018-06-30") == "30.06.2018"
+
 
 def test_get_data():
-    path = os.path.join(ROOT_DIR, "tests", "test_operation.json")
-    assert get_data(path) == [1, 2, 3]
+    assert get_data(TEST_OPERATIONS_PATH) == [1, 2, 3]
+
 
 def test_get_sorted_list():
-    data = ["2018-03-23", "2019-08-26", "2019-07-03", "2020-07-03"]
-    assert get_sorted_list(data) == ["2020-07-03", "2019-08-26", "2019-07-03", "2018-03-23"]
+    with open(TEST_OPERATIONS_SORTED_PATH) as file:
+        data = json.load(file)
+    assert get_sorted_list(data) == [{"date": "2020-07-03"},
+                                     {"date": "2019-08-26"},
+                                     {"date": "2019-07-03"},
+                                     {"date": "2018-03-23"}]
+
 
 def test_get_filtered_data():
-    dictionary = [
-        {
-            "id": 86608620,
-            "state": "EXECUTED",
-            "date": "2019-08-16T04:23:41.621065",
-            "operationAmount": {
-                "amount": "6004.00",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод с карты на счет",
-            "from": "MasterCard 8826230888662405",
-            "to": "Счет 96119739109420349721"
+    with open(TEST_OPERATIONS_FILTERED_PATH, encoding="UTF-8") as file:
+        data = json.load(file)
+    assert get_filtered_data(data) == [{
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+          "amount": "31957.58",
+          "currency": {
+            "name": "руб.",
+            "code": "RUB"
+          }
         },
-        {
-            "id": 232222017,
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+      }]
+
+
+def test_formate_operations_for_output():
+    data_1 = {
+            "id": 441945886,
             "state": "EXECUTED",
-            "date": "2018-07-06T22:32:10.495465",
+            "date": "2019-08-26T10:50:58.294041",
             "operationAmount": {
-                "amount": "37160.27",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                },
-    "description": "Перевод с карты на карту",
-    "from": "Visa Classic 4062745111784804",
-    "to": "Maestro 8602249654751155"
-    },}]
-    assert get_filtered_data(dictionary) == ["2019-08-16"]
+              "amount": "31957.58",
+              "currency": {
+                "name": "руб.",
+                "code": "RUB"
+              }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+    }
+    assert formate_operations_for_output(data_1) == ('\n26.08.2019 Перевод организации\n'
+                                                     'Maestro 1596 83** **** 5199 -> Счет **9589\n'
+                                                     '31957.58 руб.')
+    data_2 = {
+            "id": 587085106,
+            "state": "EXECUTED",
+            "date": "2018-03-23T10:45:06.972075",
+            "operationAmount": {
+              "amount": "48223.05",
+              "currency": {
+                "name": "руб.",
+                "code": "RUB"
+              }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 41421565395219882431"
+          }
+    assert formate_operations_for_output(data_2) == ('\n23.03.2018 Открытие вклада\n'
+                                                     'Anonymous -> Счет **2431\n'
+                                                     '48223.05 руб.')
